60f225f9315768c6dc908858e6fd13d88d253a7a
Belffegor <Belffegor2@gmail.com>
2021-08-03 00:49:14 +0300

    i dont now

---
+++ script.js
@@ -0,0 +1,34 @@
+// function createElem (content) { // making element 
+//     const elem = document.createElement('img');
+//     elem.href = 'https://img1.freepng.ru/20171221/tie/fire-explosion-transparent-png-clip-art-image-5a3c4361582126.049378161513898849361.jpg';
+    
+//     return elem;
+// }
+//const crElemCopy = crElem.cloneNode(true);
+
+
+// btn.onclick = function(){
+//     createElem();
+// }
+
+const click = document.getElementsByClassName('btn');
+
+click.onclick = function(){
+    //  createElem (content);
+    //  a = click;
+     console.log('aaaaa');
+    
+};
+
+// function createElem (content) { // making element 
+//     const elem = document.createElement('h1');
+    
+//     elem.textContent = content;
+//     return elem;
+// }
+// const crElem = createElem('BA-BAX');
+// const crElemCopy = crElem.cloneNode(true);
+// buttons.before(crElem);
+
+//const crElem = createElem('BA-BAX'); // elem with content
+
